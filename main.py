cee4d7a198e730d695b5540f3a6a0d367730846e
Mostafa Hosseini <s.mostafa.2001.1380@gmail.com>
2023-01-31 14:26:07 +0330

    Added console application

--- main.py
+++ main.py
@@ -1,44 +1,16 @@
 from Trie import Trie
-from tkinter import *
 
 trie = Trie()
 fileDescriptor = open("words.txt")
 words = fileDescriptor.read().split()
 trie.formTrie(words)
+compare = trie.autoSuggestions(input("Enter your query: ").lower())
+suggestions = trie.getSuggestions()
 
-window = Tk()
-window.geometry("410x400")
-window.title("پروژه ساختمان داده")
-font = ("Times", 24, "bold")
-
-
-def check(event):
-    value = entry.get()
-    if value == '':
-        data = words
-
-    else:
-        trie.autoSuggestions(value.lower())
-        data = trie.getSuggestions()
-    update(data)
-
-
-def update(data):
-    menu.delete(0, END)
-    for value in data:
-        menu.insert(END, value)
-
-
-text = Label(window, text="Enter your query")
-text.pack(padx=15, pady=20)
-entry = Entry(window, width=35)
-entry.pack()
-entry.bind('<KeyRelease>',check)
-menu = Listbox(window)
-menu.pack()
-
-update(words)
-
-window.mainloop()
-
-
+counter = 0
+for suggestion in suggestions:
+    print(suggestion, end="\t")
+    counter += 1
+    if counter == 3:
+        print()
+        counter = 0
